1628d9efff2dc763f3911e780e9e8ac367fcb0c8
Dmitry <dmrudakov@fmlogistic.com>
2022-11-25 05:37:26 +0300

    dz1 done

--- DZ1/001/Program.cs
+++ DZ1/001/Program.cs
@@ -5,10 +5,10 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.Write("Введите число a");
+Console.Write("Введите число a ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число b");
+Console.Write("Введите число b ");
 int b = Convert.ToInt32(Console.ReadLine());
 
 if (a > b) Console.WriteLine($"число {a} больше числа {b}");
