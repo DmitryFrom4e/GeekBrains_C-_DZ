bfcd3c79cd262acaa6046a0cc8e7038d3248de29
Dmitry <dmrudakov@fmlogistic.com>
2022-12-05 07:06:39 +0300

    done dz4

---
+++ DZ4/002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+int SumNums (int numIn)
+{
+    int numOut = 0;
+    while (numIn > 0)
+    {
+        numOut += numIn % 10;
+        numIn /= 10 ;
+    }
+    return numOut;
+}
+
+Console.Write("Введите число num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumNums(num));
+
