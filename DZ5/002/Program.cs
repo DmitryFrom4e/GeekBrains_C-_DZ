bdf7bcea3323128b0d352238ce49ab91fb546c34
Dmitry <dmrudakov@fmlogistic.com>
2022-12-06 07:27:02 +0300

    done dz5

---
+++ DZ5/002/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] arr = new int[10];
+int sumX = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(100, 1000);
+    Console.Write($"{arr[i]} ");
+
+    if(i % 2 != 0) sumX += arr[i];
+}
+
+Console.WriteLine();
+Console.WriteLine(sumX);
---
+++ DZ5/003/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу 
+// между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] arr = new double[10];
+double sumX = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = Math.Round(new Random().Next(1, 100) + new Random().NextDouble(), 2);
+    Console.Write($"{arr[i]} ");
+}
+
+double max = arr[0];
+double min = arr[0];
+
+for (int i = 1; i < arr.Length; i++)
+{
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
+}
+Console.WriteLine();
+Console.WriteLine($"max {max}, min {min}");
+
+Console.WriteLine(Math.Round(max - min, 2));
