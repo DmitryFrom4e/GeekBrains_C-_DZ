4d32e5f3ec4b12169eb66857730181a417eaf8e7
Dmitry <dmrudakov@fmlogistic.com>
2022-12-13 07:44:42 +0300

    done dz6

---
+++ DZ6/001/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int CountNums(int arrLen)
+{
+    int count = 0;
+    int[] arr = new int[arrLen];
+    for (int i = 0; i < arrLen; i++)
+    {
+        Console.Write("Введите число с клавиатуры: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        if (arr[i] > 0) count++;
+    }
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+    return count;
+}
+
+Console.Write("Введите кол-во чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Количество чисел > 0 {CountNums(M)}");
+
+
+
