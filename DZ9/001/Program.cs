c1994d8f967aabb3c1bb507d74e38f1854a795d3
Dmitry <dmrudakov@fmlogistic.com>
2022-12-22 22:04:13 +0300

    done dz9

---
+++ DZ9/001/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void ShowList(int num)
+{
+    if (num > 0)
+    {
+        Console.Write($"{num} ");
+        num--;
+        ShowList(num);
+    }
+}
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 0) Console.WriteLine("введите число больше 0");
+else ShowList(N);
---
+++ DZ9/002/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int sumAll = 0;
+int sum = 0;
+
+int SumElements(int numM, int numN)
+{
+    sum = sumAll;
+    if (numM <= numN)
+    {
+        sumAll += numM;
+        numM++;
+        return SumElements(numM, numN);
+    }  else return sum;
+}
+
+if (M > N) Console.WriteLine("M > N");
+else Console.WriteLine($"сумма элементов {SumElements(M, N)}");
+
